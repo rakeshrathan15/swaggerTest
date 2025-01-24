9fe042bef26cfbd17a886d446d44a2ff696baa6e
rakesh mekala <rakeshyadavmekala15@gmail.com>
2025-01-24 14:12:20 +0530

    Swagger file

---
+++ src/test/java/com/neoteric/swaggerTest/SwaggerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.swaggerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
